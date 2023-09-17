ef5a9f2a6b095d750c79d1de5ca06ad7adaaf28f
Darwin Gutierrez <darwindrum@gmail.com>
2023-09-17 18:32:50 -0500

    se ajustaron los comandos de readline

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const readline = require('node:readline');
+
 let listaDeTareas = [{
-    id: "tarea1",
+    id: "1",
     descripcion: "sacar al perro",
     estaCompletada: false
 },
 {
-    id: "tarea2",
+    id: "2",
     descripcion: "pagar la luz",
     estaCompletada: false 
 }];
@@ -14,7 +16,7 @@ function mostrarTareas (){
 }
 
 
-function añadirTarea(id, descripcion){
+function agregarTarea(id, descripcion){
 
     let nuevaTarea = {
         id:id,
@@ -26,16 +28,16 @@ function añadirTarea(id, descripcion){
    
 };
 
-añadirTarea("tarea3", "bañarme");
-añadirTarea("tarea4", "desayunar");
-añadirTarea("tarea5", "hacer ejercicio");
+agregarTarea("3", "bañarme");
+agregarTarea("4", "desayunar");
+agregarTarea("5", "hacer ejercicio");
 
 
 function eliminarTarea (id) {
     listaDeTareas = listaDeTareas.filter(tarea => tarea.id !== id )
 };
 
-eliminarTarea("tarea4");
+eliminarTarea("4");
     
 function completarTarea ( id ) {
 
@@ -43,11 +45,56 @@ function completarTarea ( id ) {
 
     if(tareaCompletada) {
         tareaCompletada.estaCompletada = true;
-    };
+    } else {
+        console.log('no se encontro la tarea');
+    }
 };
 
-completarTarea("tarea1");
-completarTarea("tarea2");
+completarTarea("1");
+completarTarea("2");
 
-mostrarTareas();
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Elige un comando: (1)mostrar tareas , (2)añadir tarea, (3)completar tarea, (4)Eliminar tarea', (respuesta) => {
+     
+     console.log(`Haz elegido ${respuesta}`)
+
+     switch (respuesta) {
+        case '1':
+            console.log(mostrarTareas())
+            break;
+            case '2':
+                rl.question('Ingrese el ID de la nueva tarea: ', (id) => {
+                    rl.question('Ingrese la descripción de la nueva tarea: ', (descripcion) => {
+                        rl.question('¿La tarea está completada? (true/false): ', (estaCompletada) => {
+                            agregarTarea(id, descripcion, estaCompletada === 'false');
+                            console.log('Tarea agregada con éxito.');
+                            rl.close();
+                        });
+                    });
+                });
+            break;
+        case '3':
+            rl.question('ingrese el id de la tarea que desea completar: ', (id)=> {
+                completarTarea( id );
+                console.log('Tarea completada con éxito.');
+                rl.close();
+            });
+            break;
+        case '4':
+            rl.question('ingrese el id de la tarea que desea eliminar: ', (id)=> {
+                eliminarTarea( id );
+                console.log('Tarea elimanada con éxito.');
+                rl.close();            
+           });
+           break;
+           default:
+            console.log('Comando no reconocido.');
+            rl.close();
+            break;
+      
+    }});

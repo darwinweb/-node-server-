c321a6708f348493823242fabc8178248a544d02
Darwin Gutierrez <darwindrum@gmail.com>
2023-09-20 13:31:21 -0500

    se realizo el cambio de cada tarea a objeto con id, descripcion y estado

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const http = require('http');
 const host = 'localhost'
 const port = 3000
 
-const arrayTareas = ['lavar', 'estudiar', 'sacar al perro', 'ir a la iglesia', 'dormir'];
+const arrayTareas = [
+    { id: '1', descripcion: 'lavar', estado: 'completado'},
+    { id: '2', descripcion: 'estudiar', estado:'pendiente' },
+    { id: '3', descripcion: 'sacar al perro',  estado: 'pendiente'},
+    { id: '4', descripcion: 'ir a la iglesia',  estado: 'pendiente'},
+    { id: '5', descripcion: 'dormir',  estado: 'pendiente'}
+];
 
 
  const server = http.createServer((req, res) => {
